21c6631db8995c2ab0674d346c58aa7c27fd5044
JonasKaruzas <EnterGitHub1>
2022-04-27 21:38:29 +0300

    added Event listener. Green Red colours

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('works');
+document.addEventListener('click', handleClick);
+
+function handleClick(e) {
+    element = e.target;
+
+    if (element.classList.contains('desk')) {
+     tableClick(element);   
+    }
+
+    if ((element.classList.contains('hour')) || (element.classList.contains('day'))) {
+        timeClick(element);   
+       }
+   
+}
+
+function tableClick(e) {
+    e.classList.toggle('red');
+}
+ 
+function timeClick(e) {
+    e.classList.toggle('green');
+}
+
