8e28faa2a94977b8b89ea2d8f2e7b3a9ab2ee12c
JonasKaruzas <EnterGitHub1>
2022-04-27 22:31:03 +0300

    Day selector

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
+const
+
 document.addEventListener('click', handleClick);
 
 function handleClick(e) {
     element = e.target;
+    // console.log(element);
+    // console.log(element.id);
+    // console.log(e);
 
     if (element.classList.contains('desk')) {
-     tableClick(element);   
+     tableClick(element);
+     deskNoUpdate(element.id);   
     }
 
-    if ((element.classList.contains('hour')) || (element.classList.contains('day'))) {
-        timeClick(element);   
-       }
-   
+    if (element.classList.contains('day')) {
+        dayClick(element);   
+    }
+
+    if (element.classList.contains('hour'))  {
+        hourClick(element);   
+    }
 }
 
 function tableClick(e) {
-    e.classList.toggle('red');
-}
- 
-function timeClick(e) {
-    e.classList.toggle('green');
+    let desks = document.getElementsByClassName("desk");
+    for (let i = 0; i < desks.length; i++) {
+        desks[i].classList.remove('selected');
+    }
+    e.classList.toggle('selected');
 }
 
+function deskNoUpdate(e) {
+    let no = document.getElementsByClassName("desk-number");
+    no[0].innerHTML =  'Desk no. '.concat(e);
+}
+
+function dayClick(e) {
+    let days = document.getElementsByClassName("day");
+    for (let i = 0; i < days.length; i++) {
+        days[i].classList.remove('selected');
+    }
+    e.classList.toggle('selected');
+
+}
+
+function hourClick(e) {
+    e.classList.toggle('green');
+}
