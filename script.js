e8f9fbe597d3ab0bd57e9a7a74aa165007a83234
JonasKaruzas <EnterGitHub1>
2022-04-29 23:21:17 +0300

    Notifications

--- script.js
+++ script.js
@@ -3,37 +3,37 @@ const reservations = {
     Monday: [8, 9, 10],
     Tuesday: [11, 12],
     Wednesday: [13, 14],
-    Thursday: [15, 16, 17],
-    Friday: [10, 14],
+    Thursday: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    Friday: [],
     Saturday: [9, 13],
-    Sunday: [8, 12]
+    Sunday: [8, 12],
   },
   2: {
     Monday: [11, 12, 17],
-    Tuesday: [13, 14],
+    Tuesday: [],
     Wednesday: [17],
-    Thursday: [13],
-    Friday: [16],
-    Saturday: [12],
-    Sunday: [10]
+    Thursday: [],
+    Friday: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    Saturday: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    Sunday: [10],
   },
   3: {
     Monday: [13],
     Tuesday: [11, 12],
     Wednesday: [],
-    Thursday: [],
+    Thursday: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
     Friday: [14],
     Saturday: [],
-    Sunday: [9]
+    Sunday: [9],
   },
   4: {
-    Monday: [11, 12, 17],
-    Tuesday: [13, 14],
+    Monday: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    Tuesday: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
     Wednesday: [11],
     Thursday: [],
     Friday: [],
     Saturday: [],
-    Sunday: []
+    Sunday: [],
   },
   5: {
     Monday: [8, 9, 10],
@@ -42,7 +42,7 @@ const reservations = {
     Thursday: [],
     Friday: [10, 14],
     Saturday: [9, 13],
-    Sunday: [8, 12]
+    Sunday: [8, 12],
   },
   6: {
     Monday: [],
@@ -51,39 +51,50 @@ const reservations = {
     Thursday: [],
     Friday: [],
     Saturday: [],
-    Sunday: []
-  }
+    Sunday: [],
+  },
 };
 
 let activeTable = 0;
 let activeDay = "";
+let r = document.querySelector(":root");
 
 document.addEventListener("click", handleClick);
 
+let a = document.getElementsByClassName("day");
+for (let i = 0; i < a.length; i++) {
+  const notificationDiv = document.createElement("div");
+  notificationDiv.className = "notification";
+  a[i].appendChild(notificationDiv);
+}
+
 function handleClick(e) {
   element = e.target;
-  // console.log(element);
-  // console.log(element.id);
-  // console.log(e);
+  console.log(element);
+  console.log(element.id);
+  console.log(e);
 
   if (element.classList.contains("desk")) {
     tableClick(element);
     deskNoUpdate(element.id);
     activateTable(element.id);
   }
-
+  
   if (element.classList.contains("day")) {
     dayClick(element);
-    activateDay(element.innerHTML);
+    activateDay(element.outerText);
   }
-
+  
   if (element.classList.contains("hour")) {
     hourClick(element);
   }
-
+  
   if (activeTable && activeDay) {
     viewReservedHours();
   }
+  
+  
+  showAvailability();
 }
 
 function tableClick(e) {
@@ -109,10 +120,15 @@ function dayClick(e) {
 
 function hourClick(e) {
   newHour = parseInt(e.innerHTML);
+
   if (reservations[activeTable][activeDay].indexOf(newHour) == -1) {
     reservations[activeTable][activeDay].push(newHour);
+  } else {
+    reservations[activeTable][activeDay].splice(
+      reservations[activeTable][activeDay].indexOf(newHour),
+      1
+    );
   }
-
 }
 
 function activateTable(tableID) {
@@ -128,6 +144,8 @@ function viewReservedHours() {
   let allHours = document.getElementsByClassName("hour");
 
   removeAllRed(allHours);
+  console.log("------------");
+  console.log(reservedHours);
 
   for (let i = 0; i < reservedHours.length; i++) {
     for (let j = 0; j < allHours.length; j++) {
@@ -136,11 +154,42 @@ function viewReservedHours() {
       }
     }
   }
-
 }
 
 function removeAllRed(redElements) {
   for (let i = 0; i < redElements.length; i++) {
     redElements[i].classList.remove("red");
   }
+}
+
+function showAvailability() {
+  rDays = Object.keys(reservations[activeTable]);
+  divDays = document.getElementsByClassName("day");
+  removeNotifications();
+
+  for (let i = 0; i < divDays.length; i++) {
+    divDayText = divDays[i].outerText;
+    reservationLength = reservations[activeTable][divDayText].length;
+
+    if (reservationLength == 0) {
+      divDays[i].children[0].classList.add("greenNotif");
+    } else if (reservationLength == 10) {
+      divDays[i].children[0].classList.add("redNotif");
+    } else {
+      divDays[i].children[0].classList.add("orange");
+    }
+  }
+}
+
+function removeNotifications() {
+  n = document.getElementsByClassName('notification');
+  
+  for (let i = 0; i < n.length; i++) {
+    const element = n[i];
+    element.classList.remove('redNotif');
+    element.classList.remove('greenNotif');
+    element.classList.remove('orange');
+    
+  }
+  
 }
